b3d18e219ac7e8a78c28ec18a87bd09efa7354a6
Chirannnn <chiranyt117@gmail.com>
2025-08-19 22:47:57 +0530

    update assignments

--- i-idea-JAVA/05-arrays/src/com/practice/Main.java
+++ i-idea-JAVA/05-arrays/src/com/practice/Main.java
@@ -1,7 +1,28 @@
 package com.practice;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // Q: store a roll number
+        int a = 19;
 
+        // Q: store a person's name
+        String name = "Chiranjeevi";
+
+        // Q: store 5 roll numbers
+        int rno1 = 23;
+        int rno2 = 55;
+        int rno3 = 18;
+
+        // syntax
+        // datatype[] variable_name = new datatype[size];
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
