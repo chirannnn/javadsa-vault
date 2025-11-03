bbd2e2622c831deea7c7166130485d61ae66d484
Chirannnn <chiranyt117@gmail.com>
2025-11-03 23:15:16 +0530

    update lecture

--- i-idea-JAVA/08-sorting/src/com/lecture/Main_01.java
+++ i-idea-JAVA/08-sorting/src/com/lecture/Main_01.java
@@ -6,8 +6,20 @@ public class Main_01 {
     public static void main(String[] args) {
         int[] arr = {5,4,3,2,1};
 
-        selection(arr);
+        insertion(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j > 0 ; j--) {
+                if (arr[j] < arr[j-1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     static void selection(int[] arr) {
@@ -44,11 +56,11 @@ public class Main_01 {
             // for each step, max item will come at the last respective index
             for (int j = 1; j < arr.length - i; j++) {
                 // swap if the item is smaller than the previous item
-                if (arr[j] < arr[j - 1]) {
+                if (arr[j] < arr[j-1]) {
                     // swap
                     int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
                     swapped = true;
                 }
             }
