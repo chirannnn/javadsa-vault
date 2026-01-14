30bb9063a5b0ddb9a5ef578ef75a1dab97ae1e9c
Chirannnn <chiranyt117@gmail.com>
2026-01-14 20:36:03 +0530

    update lecture

--- i-idea-JAVA/10-patterns/src/com/lecture/Main.java
+++ i-idea-JAVA/10-patterns/src/com/lecture/Main.java
@@ -2,7 +2,7 @@ package com.lecture;
 
 public class Main {
     public static void main(String[] args) {
-        pattern5(4);
+        pattern31(4);
     }
 
     static void pattern1(int n) {
@@ -47,6 +47,74 @@ public class Main {
 
             for (int col = 1; col <= totalColsInRow; col++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern28(int n) {
+        for (int row = 1; row < 2 * n; row++) {
+
+            int totalColsInRow = row > n ? 2 * n - row : row;
+            int noOfSpaces = n - totalColsInRow;
+
+            for (int s = 1; s <= noOfSpaces; s++) {
+                System.out.print(" ");
+            }
+
+            for (int col = 1; col <= totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern17(int n) {
+        for (int row = 1; row < 2 * n; row++) {
+
+            int totalColsInRow = row > n ? 2 * n - row : row;
+            int noOfSpaces = n - totalColsInRow;
+            for (int s = 1; s <= noOfSpaces; s++) {
+                System.out.print("  ");
+            }
+
+            for (int col = totalColsInRow; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= totalColsInRow; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern30(int n) {
+        for (int row = 1; row <= n; row++) {
+
+            int noOfSpace = n - row;
+            for (int s = 1; s <= noOfSpace; s++) {
+                System.out.print("  ");
+            }
+
+            for (int col = row; col >= 1 ; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    static void pattern31(int n) {
+        int originalN = n;
+        n = 2 * n - 1;
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                int atEveryIndex = originalN - Math.min(Math.min(row - 1, col - 1), Math.min(n - row, n - col));
+                System.out.print(atEveryIndex + " ");
             }
             System.out.println();
         }
