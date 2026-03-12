0367e3615e08ab3b4a146e2efa3fb5b3b042274c
Chirannnn <chiranyt117@gmail.com>
2026-03-12 20:02:09 +0530

    update lecture

--- i-idea-JAVA/13-oop/src/com/lecture/introduction/Main.java
+++ i-idea-JAVA/13-oop/src/com/lecture/introduction/Main.java
@@ -18,16 +18,33 @@ public class Main {
         Student[] students = new Student[5];
 
         // just declaring
-        Student chiran;
-        chiran = new Student();
+//        Student chiran;
+//        chiran = new Student();
 
 //        chiran.rno = 5;
 //        chiran.name = "Chiranjeevi D";
 //        chiran.marks = 34.99f;
 
+        Student chiran = new Student(1, "Chirrrrnnn", 10.0f);
+
+//        chiran.changeName("Kalai");
+//        chiran.greeting();
+
         System.out.println(chiran.rno);
         System.out.println(chiran.name);
         System.out.println(chiran.marks);
+
+        Student random = new Student(chiran);
+        System.out.println(random.name);
+
+        Student random2 = new Student();
+        System.out.println(random2.name);
+
+        Student one = new Student();
+        Student two = one;
+
+        one.name = "Maid";
+        System.out.println(two.name);
     }
 }
 
@@ -35,4 +52,38 @@ class Student {
     int rno;
     String name;
     float marks = 95.0f;
+
+    // we need a way to add the values of the above
+    // properties object by object
+
+    // we need one word to access every object
+
+    void greeting () {
+        System.out.println("Hello My name is " + this.name);
+    }
+
+    void changeName (String name) {
+        this.name = name;
+    }
+
+    Student (Student other) {
+        this.rno = other.rno;
+        this.name = other.name;
+        this.marks = other.marks;
+    }
+
+    Student () {
+        // this is how you call a constructor from another constructor
+        // internally: new Student (13, "default person", 100.0f);
+        this (13, "random student", 45.0f);
+    }
+
+
+    // Student arpit = new Student(17, "Arpit", 89.7f);
+    // here, this will be replaced with arpit
+    Student (int rno, String name, float marks) {
+        this.rno = rno;
+        this.name = name;
+        this.marks = marks;
+    }
 }
