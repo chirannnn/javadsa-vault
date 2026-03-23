be930a9ec54aade44b1eb7807d68487ae36bd483
Chirannnn <chiranyt117@gmail.com>
2026-03-23 23:51:24 +0530

    update oop

---
+++ i-idea-JAVA/13-oop/src/com/lecture/staticExample_03/InnerClasses.java
@@ -0,0 +1,31 @@
+package com.lecture.staticExample_03;
+
+public class InnerClasses {
+
+    static class Test {
+        String name;
+
+        public Test(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    public static void main(String[] args) {
+        Test a = new Test("Priya");
+        Test b = new Test("Malar");
+
+        System.out.println(a);
+
+//        System.out.println(a.name);
+//        System.out.println(b.name);
+    }
+}
+
+//static class A {
+//
+//}
