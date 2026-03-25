0fcb4e0292104026f9255c09e8e640e2fb94ef45
Chirannnn <chiranyt117@gmail.com>
2026-03-25 20:45:53 +0530

    update oop

---
+++ i-idea-JAVA/13-oop/src/com/lecture/properties_05/inheritance/Main.java
@@ -0,0 +1,14 @@
+package com.lecture.properties_05.inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+//        Box box1 = new Box(1, 2, 3);
+//        System.out.println(box1.l);
+//
+//        Box box2 = new Box(box1);
+//        System.out.println(box1.l);
+
+        BoxWeight box3 = new BoxWeight();
+        System.out.println(box3.l);
+    }
+}
