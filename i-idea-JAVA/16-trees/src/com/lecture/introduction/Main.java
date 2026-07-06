5cc349273b0201b092d957e4e97c88c7fa0ab28c
Chirannnn <chiranyt117@gmail.com>
2026-07-07 02:35:28 +0530

    update BSTree

--- i-idea-JAVA/16-trees/src/com/lecture/introduction/Main.java
+++ i-idea-JAVA/16-trees/src/com/lecture/introduction/Main.java
@@ -2,9 +2,15 @@ package com.lecture.introduction;
 
 public class Main {
     public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
-        tree.populate();
+//        BinaryTree tree = new BinaryTree();
+//        tree.populate();
 //        tree.display();
-        tree.prettyDisplay();
+//        tree.prettyDisplay();
+
+//        int[] values = {3, 7, 5, 9, 0, 1};
+        int[] values = {1, 2, 3, 4, 5, 6, 7, 8};
+        BST tree = new BST();
+        tree.populateSorted(values);
+        tree.display();
     }
 }
