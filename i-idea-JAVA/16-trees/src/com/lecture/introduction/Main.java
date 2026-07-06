83f84af03ae963725e51639a86775cfb5fef394b
Chirannnn <chiranyt117@gmail.com>
2026-07-06 20:53:49 +0530

    update trees

---
+++ i-idea-JAVA/16-trees/src/com/lecture/introduction/Main.java
@@ -0,0 +1,10 @@
+package com.lecture.introduction;
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.populate();
+//        tree.display();
+        tree.prettyDisplay();
+    }
+}
